e22864db0a57260148f75761c26bb381add13a63
Natalia Frolova <addict7@yandex.ru>
2022-10-18 20:59:51 +0300

    new_commit

---
+++ conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from selenium import webdriver
+import time
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    driver.maximize_window()
+    driver.get("https://stellarburgers.nomoreparties.site/")
+    time.sleep(3)
+    return driver
+
---
+++ tests/test_wrong_password.py
@@ -0,0 +1,23 @@
+def test_wrong_password(driver):
+
+    from selenium.webdriver.common.by import By
+    import time
+
+    driver.find_element(By.XPATH, "/html/body/div/div/header/nav/a").click()
+
+    driver.find_element(By.XPATH, "/html/body/div/div/main/div/div/p[1]/a").click()
+
+    driver.find_element(By.XPATH, "/html/body/div/div/main/div/form/fieldset[1]/div/div/input").send_keys("Наталья")
+
+    driver.find_element(By.XPATH, "/html/body/div/div/main/div/form/fieldset[2]/div/div/input").send_keys(
+        "nataliafrolov32435@ya.ru")
+
+    driver.find_element(By.XPATH, "/html/body/div/div/main/div/form/fieldset[3]/div/div/input").send_keys("00001")
+
+    driver.find_element(By.XPATH, "/html/body/div/div/main/div/form/button").click()
+
+    time.sleep(5)
+
+    assert driver.find_element(By.XPATH,"/html/body/div/div/main/div/form/fieldset[3]/div/p").text == 'Некорректный пароль'
+
+    driver.quit()
