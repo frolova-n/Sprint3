413b91aea7786860c9ab3c6e87a206c813880d5b
Natalia Frolova <addict7@yandex.ru>
2022-10-23 04:45:15 +0300

    new_commit

--- tests/test_login_account.py
+++ tests/test_login_account.py
@@ -1,19 +1,69 @@
-from selenium.webdriver.common.by import By
+from locators import Authorization
 import time
+from methods import LoginPageBurger
 
 def test_login_account(driver):
 
-    driver.find_element(By.XPATH, "/html/body/div/div/main/section[2]/div/button").click()
+    driver.find_element(*Authorization.login_button).click()
 
-    driver.find_element(By.NAME, "name").send_keys("nataliafrolova3678@ya.ru")
+    login_page = LoginPageBurger(driver)
 
-    driver.find_element(By.NAME, "Пароль").send_keys("000018")
-
-    driver.find_element(By.XPATH, "/html/body/div/div/main/div/form/button").click()
+    login_page.login("nataliafrolova3678@ya.ru", "000018")
 
     time.sleep(3)
 
-    assert driver.find_element(By.XPATH, "/html/body/div/div/main/section[2]/div/button").text == 'Оформить заказ'
+    assert driver.find_element(*Authorization.make_an_order_button).text == 'Оформить заказ'
+
+    driver.quit()
+
+def test_login_personal_cab(driver):
+
+    driver.find_element(*Authorization.personal_cab_button).click()
+
+    login_page = LoginPageBurger(driver)
+
+    login_page.login("nataliafrolova3678@ya.ru", "000018")
+
+    time.sleep(3)
+
+    assert driver.find_element(*Authorization.make_an_order_button).text == 'Оформить заказ'
+
+    driver.quit()
+
+def test_login_reg_form(driver):
+
+
+    driver.find_element(*Authorization.personal_cab_button).click()
+
+    driver.find_element(*Authorization.registration_form_button).click()
+
+    driver.find_element(*Authorization.login_registration_form_button).click()
+
+    login_page = LoginPageBurger(driver)
+
+    login_page.login("nataliafrolova3678@ya.ru", "000018")
+
+    time.sleep(3)
+
+    assert driver.find_element(*Authorization.make_an_order_button).text == 'Оформить заказ'
+
+    driver.quit()
+
+def test_login_password_recovery_button(driver):
+
+    driver.find_element(*Authorization.login_button).click()
+
+    driver.find_element(*Authorization.recovery_password_button).click()
+
+    driver.find_element(*Authorization.login_registration_form_button).click()
+
+    login_page = LoginPageBurger(driver)
+
+    login_page.login("nataliafrolova3678@ya.ru", "000018")
+
+    time.sleep(3)
+
+    assert driver.find_element(*Authorization.make_an_order_button).text == 'Оформить заказ'
 
     driver.quit()
 
--- tests/test_registration.py
+++ tests/test_registration.py
@@ -1,24 +1,44 @@
-from selenium.webdriver.common.by import By
+from locators import Authorization
 import time
-
 
 def test_registration(driver):
 
-    driver.find_element(By.XPATH, "/html/body/div/div/header/nav/a").click()
+    driver.find_element(*Authorization.login_button).click()
 
-    driver.find_element(By.XPATH, "/html/body/div/div/main/div/div/p[1]/a").click()
+    driver.find_element(*Authorization.registration_form_button).click()
 
-    driver.find_element(By.XPATH, "/html/body/div/div/main/div/form/fieldset[1]/div/div/input").send_keys("Наталья")
+    driver.find_element(*Authorization.name_registration_form_field).send_keys("Наталья")
 
-    driver.find_element(By.XPATH, "/html/body/div/div/main/div/form/fieldset[2]/div/div/input").send_keys(
-        "nataliafrolova3678@ya.ru")
+    driver.find_element(*Authorization.email_registration_form_field).send_keys("nataliafrolova3678@ya.ru")
 
-    driver.find_element(By.XPATH, "/html/body/div/div/main/div/form/fieldset[3]/div/div/input").send_keys("000018")
+    driver.find_element(*Authorization.password_registration_form_field).send_keys("000018")
 
-    driver.find_element(By.XPATH, "/html/body/div/div/main/div/form/button").click()
+    driver.find_element(*Authorization.register_button).click()
 
     time.sleep(5)
 
     assert driver.current_url == 'https://stellarburgers.nomoreparties.site/login'
 
     driver.quit()
+
+
+def test_wrong_password(driver):
+
+    driver.find_element(*Authorization.login_button).click()
+
+    driver.find_element(*Authorization.registration_form_button).click()
+
+    driver.find_element(*Authorization.name_registration_form_field).send_keys("Наталья")
+
+    driver.find_element(*Authorization.email_registration_form_field).send_keys("nataliafrolov32435@ya.ru")
+
+    driver.find_element(*Authorization.password_registration_form_field).send_keys("00001")
+
+    driver.find_element(*Authorization.register_button).click()
+
+    time.sleep(3)
+
+    assert driver.find_element(*Authorization.wrong_password_header).text == 'Некорректный пароль'
+
+    driver.quit()
+
